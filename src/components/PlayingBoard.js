ac06f29ef25f2e55e0df3106b61459f389b54670
cramerj1470 <john@Cramerec.com>
2022-12-09 14:26:37 -0500

    smaller for cell phones

--- src/components/PlayingBoard.js
+++ src/components/PlayingBoard.js
@@ -7,19 +7,19 @@ const blocks = ["LTC","FW", "LBC", "BlockSV", "BlockSH", "Empty", "RBC", "RTC"];
 const PlayingBoard = () => {
 	//console.log(blocks);
 	let rows = [];
-	for (let r = 0; r < 5; r++) {
+	for (let r = 0; r < 4; r++) {
 		//console.log('in the r loop');
 		let newRow = [];
 		let y;
-		for (let x = 0; x < 10; x++) {
+		for (let x = 0; x < 5; x++) {
 			let randomPick = Math.floor(Math.random() * 7);
 			//console.log(randomPick);
 			//console.log(blocks[randomPick]);
 			newRow[x] = blocks[randomPick];
 			//console.log(rows[r][x]);
 			console.log(newRow);
-			if (r===0) {newRow[0] = "LTC";newRow[9]= "RTC"}
-			if (r===4) {newRow[0] = "LBC";newRow[9]= "RBC"}
+			if (r===0) {newRow[0] = "LTC";newRow[4]= "RTC"}
+			if (r===3) {newRow[0] = "LBC";newRow[4]= "RBC"}
 			 
 			
 		}
