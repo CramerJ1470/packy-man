062e25fce4b953b36fed9c1653ae1a98ad0e5738
cramerj1470 <john@Cramerec.com>
2022-12-31 02:26:11 -0500

    working on column movement

--- src/components/BottDE.js
+++ src/components/BottDE.js
@@ -1,23 +1,20 @@
 import "../index.css";
-import RightDE from "./RightDE";
 import StroopWafels from "./StroopWafels";
 
 const BottDE = () => {
 	return (
 		<>
-			
-				<div id="a1" className="block ">
+			<div id="a1" className="block ">
 				<div className="block rot270">
-				<div className="line line1 rder"></div>
-				<div className="line line2 rder"></div>
-				<div className="line line1 top2"></div>
-				<div className="line line2 top2 "></div>
-				<div className="line line1 top2 dropBottom"></div>
-				<div className="line line1 top2 dropBottom rot180 "></div>
-					</div>
-					<StroopWafels block={"BottDE"}/>
+					<div className="line line1 rder"></div>
+					<div className="line line2 rder"></div>
+					<div className="line line1 top2"></div>
+					<div className="line line2 top2 "></div>
+					<div className="line line1 top2 dropBottom"></div>
+					<div className="line line1 top2 dropBottom rot180 "></div>
 				</div>
-				
+				<StroopWafels block={"BottDE"}/>
+			</div>
 		</>
 	);
 };
--- src/components/Row.js
+++ src/components/Row.js
@@ -4,42 +4,46 @@ import "../index.css";
 import Eachone from "./Eachone";
 //import updateAllRows from "./PlayingBoard";
 
-// let allColumns = [];
-//let allRows= [];
+ let allColumns = [];
+let allRows= [];
 const Row = ({ row, blocks,index }) => {
-//	let newColumn =[];
-//	let colStart;
-//	let colEnd;
+	let newColumn =[];
+	let colStart;
+	let colEnd;
+	let start;
+	let end;
 	
-//	let newRow = [];
-//	let end;
+	let newRow = [];
+	console.log(`blocks:`,blocks);
+ 
 //**********Create data that will be used by characters for moving based on the game board************/
 
 // *****************on a given Y the start-end of each row path  **********************
-/*	for (let ay = 1; ay < row.length - 1; ay++) {
+	for (let ay = 1; ay < row.length - 1; ay++) {
 		let currentBlock = row[ay];
+		//console.log(`currentBlock.x:`,currentBlock.x);
 		
 		if (
 			[ "CornBord", "vertBord", "HorizBord","TopDE","BottDE"].includes(
 				currentBlock.blockName
 			) === false
-		) {console.log(`currentBlock:`,currentBlock);
-			console.log(`cur x; `, currentBlock.x);
+		) {//console.log(`currentBlock:`,currentBlock);
+			//console.log(`cur x; `, currentBlock.x);
 			if (currentBlock.x > 0) {
 				if (
 					blocks[currentBlock.x - 1].properties.c === "closed" &&
 					currentBlock.properties.c === "open"
 				) {
-					console.log(`currentBlock: `, currentBlock);
-					start = currentBlock.positions.horiz.b.x * 16;
-					console.log(`start: `, start);
+					//console.log(`currentBlock: `, currentBlock);
+					start = currentBlock.xCenter;
+					//console.log(`start: `, start);
 				} else if (
 					blocks[currentBlock.x - 1].properties.c === "open" &&
 					currentBlock.properties.c === "closed"
 				) {
-					end = currentBlock.positions.horiz.b.x * 16; //sets offsetLeft
-					console.log(`end: `, end);
-					let z = currentBlock.positions.horiz.b.y * 16; //sets offsetTop
+					end = currentBlock.xCenter; //sets offsetLeft
+					//console.log(`end: `, end);
+					let z = currentBlock.yCenter; //sets offsetTop
 
 					//*************look for matching y then just push x
 					newRow.push({ y: z, x: [{ start: start, end: end }] });
@@ -47,12 +51,12 @@ const Row = ({ row, blocks,index }) => {
 					
 				}
 			}
-			console.log(`newRow after set: `, newRow);
+			//console.log(`newRow after set: `, newRow);
 		}
 		
 	}
 
-	console.log(`index:`,index, `newRow: `,newRow);
+	//console.log(`index:`,index, `newRow: `,newRow);
 	if (newRow.length >0) {
 	allRows.push(newRow);}
 	
@@ -60,55 +64,65 @@ const Row = ({ row, blocks,index }) => {
 	for (let ind = 0; ind< allRows.length; ind++) { if (ind%2 ===0) {
 		rowsToKeep.push(allRows[ind]);
 	}
-	console.log(rowsToKeep);
+	//console.log(rowsToKeep);
 	}
+
 // *****************on a given X the start-end of each column path  **********************
-/*for (let cl = 12; cl < 21; cl++){   // starting Block
-	if (blocks[x].blockName !== "Empty") {
+for (let cl = 12; cl < 21; cl++){   // starting Block
+	
+	if (blocks[cl].blockName !== "Empty") {
 		for (let nextCol = cl+0; nextCol < 45;nextCol=nextCol+11){//********************* 
 	let currentBlock = blocks[nextCol];
+	console.log(`currentBlock nextCol:`,currentBlock);
 	if ([ "CornBord", "vertBord", "HorizBord","TopDE","BottDE"].includes(
 			currentBlock.blockName
 		) === false
-	) {console.log(`currentBlock:`,currentBlock);
-		console.log(`cur x; `, currentBlock.x);
+	) {//console.log(`currentBlock:`,currentBlock);
+		//console.log(`cur y; `, currentBlock.y);
 		if (currentBlock.x > 0 ) {
 			if (
 				blocks[currentBlock.x - 11].properties.d === "closed" &&
 				currentBlock.properties.b === "open"
 			) {
-				console.log(`currentBlock: `, currentBlock);
-				start = currentBlock.positions.vert.f.y * 16;
-				console.log(`start: `, start);
+				//console.log(`currentBlock: `, currentBlock);
+				start = currentBlock.yCenter;
+				//console.log(`start: `, start);
 			} else if (
 				blocks[currentBlock.x - 11].properties.d === "open" &&
 				currentBlock.properties.c === "closed"
 			) {
-				end = currentBlock.positions.horiz.b.x * 16; //sets offsetLeft
-				console.log(`end: `, end);
-				let z = currentBlock.positions.horiz.b.y * 16; //sets offsetTop
+				end = currentBlock.yCenter; //sets offsetLeft
+				//console.log(`end: `, end);
+				let z = currentBlock.xCenter; //sets offsetTop
 
 				//*************look for matching y then just push x
-				newRow.push({ y: z, x: [{ start: start, end: end }] });
+				newRow.push({ x: z, y: [{ start: start, end: end }] });
 
 				
 			}
 		}
-		console.log(`newRow after set: `, newRow);*
+		//console.log(`newRow after set: `, newRow);
 	}
 	
 }
 }
+}
+
+
 console.log(`index:`,index, `newRow: `,newRow);
 if (newRow.length >0) {
-allRows.push(newRow);}
+	allRows.push(newRow);}
 
-let rowsToKeep= [];
+
 for (let ind = 0; ind< allRows.length; ind++) { if (ind%2 ===0) {
 	rowsToKeep.push(allRows[ind]);
 }
+}
+
+
+
 console.log(rowsToKeep);
-}*/
+
 
 	 
 	
